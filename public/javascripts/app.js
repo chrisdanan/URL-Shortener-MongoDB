792fb4d47b48764359626fc7cd7b5ab38dc8f4d0
Christopher Danan <chrisddan@gmail.com>
2015-04-14 19:53:03 -0700

    Add DOM elements for allowing the user to input URLs.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,17 @@
 var main = function(){
 	"use strict";
 
-	console.log("Hello Vane!");
+	//DOM elements for the area where user can input URLs.
+	//DOM elements for the area where user can input URLs.
+	var $inputURL = $("<input>").attr({"id": "inputURLText", "name": "inputURL"}),
+		$URLSubmitBtn = $("<button>").text("Submit").attr("id", "inputURLBtn"),
+		$tagLabel = $("<label>").text("Enter URL here").attr("for", "inputURL"),
+		$resultParagraph = $("<p>").attr("id", "resultParagraph");
+
+	$("#inputURLDiv").append($tagLabel);
+	$("#inputURLDiv").append($inputURL);
+	$("#inputURLDiv").append($URLSubmitBtn);
+
 };
 
 $(document).ready(main);
